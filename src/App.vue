a3dc4f2d440400d3815ce1bd996965a805191dc9
fileofish <oleg.b9113@gmail.com>
2023-08-15 01:41:46 +0600

    feat: add TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <the-header></the-header>
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
