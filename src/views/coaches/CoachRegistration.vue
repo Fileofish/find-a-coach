61520d3a090f727d3171d9632cd04ff2a8b6ead2
fileofish <oleg.b9113@gmail.com>
2023-08-16 04:12:33 +0600

    feat: add coach form

--- src/views/coaches/CoachRegistration.vue
+++ src/views/coaches/CoachRegistration.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <p>Coach Registration</p>
-  </div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default defineComponent({
+  components: { CoachForm },
+});
+</script>
